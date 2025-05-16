f03d71c4bf0ba64d40bad2a6770422eac46f86d0
Rohan Karanjit <karanjitrohan@gmail.com>
2025-05-16 16:45:25 +0545

    Survived or not plotted

--- titanic.py
+++ titanic.py
@@ -1,6 +1,19 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
 
-test = pd.read_csv("test.csv")
-print(test.info())
-print(test.head())
-print(test.describe())
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+sns.countplot(data=train , x="Survived")
+plt.title("Survival Count")
+plt.show()
+
+sns.countplot(data=train, x='Pclass', hue='Survived')
+plt.title("Survival by Passenger Class")
+plt.show()
+
+sns.histplot(data=train, x='Age', hue='Survived', kde=True)
+plt.title("Age Distribution by Survival")
+plt.show()
+
